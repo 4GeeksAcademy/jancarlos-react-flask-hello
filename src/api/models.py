50103608814953cec4c43bde7b3320d869e0b7c5
JancarlosT <138736732+JancarlosT@users.noreply.github.com>
2023-12-01 01:39:47 +0000

    registrolisto

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    user_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +18,12 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "user_name": self.user_name,
             "email": self.email,
+            "address": self.address,
+            "phone": self.phone,
+            "is_admin": self.is_admin,
+
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import json
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/singup', methods=['POST'])
+def create_one_user():
+    body = json.loads(request.data)
+    new_user = User(
+        name = body["name"],
+        email = body["email"],
+        password = body["password"],
+        user_name = body["user_name"],
+        address = body["address"],
+        phone = body["phone"],
+        is_admin = True
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created succesfull"}), 200
+
+
+@api.route('/user', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    if len(users) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_users = list(map(lambda x: x.serialize(), users))
+    return serialized_users, 200
+
+
+    
